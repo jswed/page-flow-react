2dbfbe0296b14ba475348bd63c9ea85cebbc526e
Garry Yao <yaojun85@gmail.com>
2014-11-05 12:03:12 +0800

    inverse of more directions

--- lib/app.jsx
+++ lib/app.jsx
@@ -4,6 +4,22 @@ var Page1 = require('./page1');
 var Menu = require('./menu');
 var Transition = React.addons.TransitionGroup;
 
+var REGEX_BACK = /(?!From)(\w*?)(Left|Top|Up)/;
+var REGEX_FORWARD = /(?!From)(\w*?)(Right|Bottom|Down)/;
+
+var REVERSE = {
+    'Left':'Right',
+    'Right':'Left',
+    'Top':'Bottom',
+    'Bottom': 'Top',
+    'Up': 'Down',
+    'Down':'Up'
+};
+
+function inverse(match, keep, dir) {
+    return keep + (REVERSE[dir] || dir);
+}
+
 module.exports = React.createClass({
     getInitialState: function(){
        return {
@@ -26,7 +42,7 @@ module.exports = React.createClass({
     },
     componentWillUpdate: function (nextProps, nextState) {
        if(nextState){
-           nextState.dir = nextState.index > this.state.index? 'rtl':'ltr';
+           nextState.dir = nextState.index > this.state.index? 'forward':'back';
        }
     },
     switchTransition: function (nextTransit) {
@@ -41,18 +57,19 @@ module.exports = React.createClass({
         var transit = this.state.pageTransition;
         var dir = this.state.dir;
 
-        if (dir === 'ltr') {
-            transit.leave = transit.leave.replace('Left', 'Right');
-            transit.enter = transit.enter.replace('Left', 'Right');
-        } else if (dir === 'rtl') {
-            transit.leave = transit.leave.replace('Right', 'Left');
-            transit.enter = transit.enter.replace('Right', 'Left');
+        if (dir === 'forward') {
+            var leave = transit.leave;
+            transit.leave = transit.leave.replace(REGEX_BACK, inverse);
+            transit.enter = transit.enter.replace(REGEX_BACK, inverse);
+        } else if (dir === 'back') {
+            transit.leave = transit.leave.replace(REGEX_FORWARD, inverse);
+            transit.enter = transit.enter.replace(REGEX_FORWARD, inverse);
         }
 
         return (
-            <section>
+            <section className="pt-perspective">
                 <Transition>
-                    <Page1 key={index} seq={index + 1} transition={transit}/>
+                    <Page1 key={index} seq={index+1} transition={transit}/>
                 </Transition>
                 <div className ="pt-triggers">
                     <button className="pt-touch-button" disabled={index === 0} onClick={this.prev}>←</button>
