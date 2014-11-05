f1471a00ed87bc5ef6dd5e568a98d9e82ca8690e
Garry Yao <yaojun85@gmail.com>
2014-11-05 09:29:46 +0800

    transition controlled by select menu

--- lib/app.jsx
+++ lib/app.jsx
@@ -1,6 +1,7 @@
 var React = require('react/addons');
 var flow = require('./flow');
 var Page1 = require('./page1');
+var Menu = require('./menu');
 var Transition = React.addons.TransitionGroup;
 
 module.exports = React.createClass({
@@ -28,15 +29,22 @@ module.exports = React.createClass({
            nextState.dir = nextState.index > this.state.index? 'rtl':'ltr';
        }
     },
+    switchTransition: function (nextTransit) {
+        console.info('new transi', nextTransit);
+        this.setState({
+            pageTransition: nextTransit
+        });
+        this.next();
+    },
     render: function () {
         var index = this.state.index;
         var transit = this.state.pageTransition;
         var dir = this.state.dir;
 
-        if(dir === 'ltr'){
+        if (dir === 'ltr') {
             transit.leave = transit.leave.replace('Left', 'Right');
             transit.enter = transit.enter.replace('Left', 'Right');
-        } else if( dir === 'rtl') {
+        } else if (dir === 'rtl') {
             transit.leave = transit.leave.replace('Right', 'Left');
             transit.enter = transit.enter.replace('Right', 'Left');
         }
@@ -44,11 +52,13 @@ module.exports = React.createClass({
         return (
             <section>
                 <Transition>
-                    <Page1 key={index} seq={index+1} transition={transit}/>
+                    <Page1 key={index} seq={index + 1} transition={transit}/>
                 </Transition>
                 <div className ="pt-triggers">
                     <button className="pt-touch-button" disabled={index === 0} onClick={this.prev}>←</button>
-                    <button className="pt-touch-button" disabled={index === flow.length - 1} onClick={this.next}>→</button>
+                &nbsp;
+                    <button className="pt-touch-button" disabled={index === 5} onClick={this.next}>→</button>
+                    <Menu onChange={this.switchTransition} />
                 </div>
             </section>
             );
